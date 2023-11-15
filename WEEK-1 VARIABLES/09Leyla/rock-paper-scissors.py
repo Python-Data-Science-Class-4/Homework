6f1ba932cce9331bc7eb59cfb7d0ed5da196e8c2
leyla060121 <leylaabrahimova91@gmail.com>
2023-11-15 20:59:31 +0100

    trial3
    done

--- WEEK-1 VARIABLES/09Leyla/rock-paper-scissors.py
+++ WEEK-1 VARIABLES/09Leyla/rock-paper-scissors.py
@@ -8,58 +8,58 @@ stone=2
 scissors=3
 sayac=10
 print(" Welcome to our game!\n For paper click  1 \nFor stone click  2 \nFor scissors click 3\n")
-while True:
-      for x in range (10):
+for x in range (0,10):
         player1=int(input("Player 1: "))
         player2=int(input("Player 2: "))
         if(player1==paper and player2==paper): #paper&paper
             print("You equal, please try again")
             sayac-=1
-            if sayac==0:False
+            #if sayac==0:status=False
 
         elif(player1==stone and player2==stone): #stone&stone
             print("You equal, please try again")
             sayac-=1
-            if sayac==0:False
+            #if sayac==0:False
 
         elif(player1==scissors and player2==scissors): #scissors&scissors
             print("You equal, please try again")
             sayac-=1
-            if sayac==0:False
+            #if sayac==0:False
       #_______________________________________________________
 
         elif player1==paper and player2==stone: #paper&stone
             print("Win Player 1")
             sayac-=1
-            if sayac==0:False
+            #if sayac==0:False
 
         elif player1==stone and player2==paper:  #stone&paper
             print("Win Player 2")
             sayac-=1
-            if sayac==0:False
+            #if sayac==0:False
 
         elif player1==scissors and player2==paper: #scissors&paper
             print("Win Player 2")
             sayac-=1
-            if sayac==0:False
+            #if sayac==0:False
 
         elif player1==paper and player2==scissors:#paper&scissors
             print("Win player 2")
             sayac-=1
-            if sayac==0:False
+            #if sayac==0:False
 
         elif player1==stone and player2==scissors:#stone&scissors
             print("Win player 1")
             sayac-=1
-            if sayac==0:False
+            #if sayac==0:False
 
         elif  player1==scissors and player2==stone: #scissors&stone
                print("Win player 2")
                sayac-=1
-               if sayac==0:False
+               #if sayac==0:False
 
         else:
             print("Please make your choose") 
-            if sayac==0:False
+            
+            #if sayac==0:False
 
         #problem: my code is not stopped after 10 times

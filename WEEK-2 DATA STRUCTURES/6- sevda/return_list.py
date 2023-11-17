dfb6c93d637b8db7248b84aacd78ca9bf65e1b48
Sevda2011 <149724906+Sevda2011@users.noreply.github.com>
2023-11-18 00:41:06 +0100

    Update by sevda

--- WEEK-2 DATA STRUCTURES/6- sevda/return_list.py
+++ WEEK-2 DATA STRUCTURES/6- sevda/return_list.py
@@ -1,3 +1,7 @@
+'''rite a program that takes two inputs; one of them is a list and the other is a number,
+and returns the list obtained by shifting the elements in the list n places to the right (left) when n is positive (negative).
+Use wrap-around if an element is shifted beyond the end of the list, then continue to shift starting at the beginning of the list.
+Example Inputs [1, 2, 3, 4, 5], 2 Output [4, 5, 1, 2, 3] Inputs [1, 2, 3, 4, 5], -2 Output [3, 4, 5, 1, 2]'''
 
 
 list_1 = []

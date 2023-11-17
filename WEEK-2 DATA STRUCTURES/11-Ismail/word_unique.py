ec3d636b9c77c920dd54616abe1dee426678b7c1
sumeyrazenkin <119689105+sumeyrazenkin@users.noreply.github.com>
2023-11-17 17:32:19 +0100

    Updated by sumeyra

--- WEEK-2 DATA STRUCTURES/11-Ismail/word_unique.py
+++ WEEK-2 DATA STRUCTURES/11-Ismail/word_unique.py
@@ -21,3 +21,9 @@ letters=(sharedletters,differenceletters1,differenceletters2)
 result= sorted((letters),key=letters.index) #convert a list and sort 
 
 print ( result)
+
+''' Kod gayet guzel ama bir kac oneri..
+**Kullanıcının girdilerini doğrudan küçük harfe çevirerek (.lower() ile) hepsini küçük harfle işleyip biraz daha güvenli hale getirebiliriz.
+**sorted() fonksiyonu kullanarak sonuçları alfabetik sıraya sokabiliriz.Sadece sonuc kisminda degil de islem yaptiginiz yerde de kullanmaniz gerekiyor.
+**Kodu daha açıklanabilir ve Pythonic hale getirmek için değişken adlarını değiştirebiliriz.
+ornegin sharedletters yerine shared_letters gibi.'''

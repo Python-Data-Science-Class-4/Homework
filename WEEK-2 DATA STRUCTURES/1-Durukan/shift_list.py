3bdfa88c2c3527faa0111ce0b06541a9f459c979
sumeyrazenkin <119689105+sumeyrazenkin@users.noreply.github.com>
2023-11-17 15:36:46 +0100

    Updated by sumeyra

--- WEEK-2 DATA STRUCTURES/1-Durukan/shift_list.py
+++ WEEK-2 DATA STRUCTURES/1-Durukan/shift_list.py
@@ -11,3 +11,20 @@ def shift_list(lst, n):
         print(lst[(shift*-1):] + lst[:(shift*-1)])
 
 shift_list(lst=(input("Enter a comma seperated list (eg.: 1,2,3): ")).split(","), n=int(input("Enter the number of shift: ")))
+
+'''Kod cok basarili
+Sadece oneri olarak sunlari soyleyebilirim.
+**shift değerini direkt olarak negatif bir değere çarpmak yerine, 
+lst[shift:] + lst[:shift] ve lst[(len(lst) - shift):] + lst[:(len(lst) - shift)] ifadelerini kullanarak daha acik ifade edebiliriz.
+**Listenin elemanlarini tek tek alabilirsiniz kullanicidan
+list_1 = []
+while True:
+    
+    inp_1 = input('Lutfen sayi girin: ')
+    
+    if inp_1 == 'q' or inp_1 == 'Q':
+        break
+    
+    list_1.append(int(inp_1))
+    Duzeltmeye gerek yok sadece bir kac oneri. Ellerinize saglik..
+'''

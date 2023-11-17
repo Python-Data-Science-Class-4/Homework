c05c252190e0253a3494143a067075341363445b
Sevda2011 <149724906+Sevda2011@users.noreply.github.com>
2023-11-18 00:27:57 +0100

    Update dictionary usege.py

--- WEEK-2 DATA STRUCTURES/6- sevda/dictionary usege.py
+++ WEEK-2 DATA STRUCTURES/6- sevda/dictionary usege.py
@@ -5,22 +5,17 @@ A two-column table containing letters and their numbers appears, with the letter
 
 sentence = input('Write a sentence:')
 sentence1 = sentence.lower()          # With Lower, capital letters in the sentence are ignored.
-new_sentence = sentence1.translate(str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
+letter = sentence1.translate(str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
 #  Punctuation marks in the sentence are ignored with 'str.maketrans'...
 
-print(new_sentence)
-a = list(sorted(new_sentence.replace(' ','')))   # Gaps in the sentence are ignored.(with 'replace')
-print((new_sentence.replace(' ','')))
+print(letter)
+letters = list(letter.replace(' ',''))   # Gaps in the sentence are ignored.(with 'replace')
+print((letter.replace(' ','')))
 
-listlen = len(a)
-list2 = list(a)
-
-print(list2)
-
-list1 = [i for i in range(1,listlen+1)]   # With the for loop and range, numbers are listed as long as the length of the list.
-matched_list = [(list1[j],list2[j]) for j in range(listlen) ]
-# Two lists are compared and their elements are matched, each letter matching a number.
-print(matched_list)
+letters_set = set(letters)
+letters_count = [(letter, letters.count(letter))for letter in letters_set]
+letters_count.sort()
+print(letters_count)
 
 
 ''' Kod guzel ama sizden isteneni vermiyor. Mesela soyle bir cumle yazdim;

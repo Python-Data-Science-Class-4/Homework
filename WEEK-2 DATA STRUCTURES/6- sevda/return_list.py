a2588f72e53990ec959007848f5866db5905cd91
Sevda2011 <149724906+Sevda2011@users.noreply.github.com>
2023-11-18 00:37:52 +0100

    Update return_list.py

--- WEEK-2 DATA STRUCTURES/6- sevda/return_list.py
+++ WEEK-2 DATA STRUCTURES/6- sevda/return_list.py
@@ -12,20 +12,14 @@ while True :
 print(list_1)
 while True :    
 
-    n = int(input('Enter a number + (positive) or - (negative) :'))  # To scroll the list right / left..
+    n = int(input('Enter a number + (positive) or - (negative)To rotate the list right or left :')) 
+     # To scroll the list right / left..
 
-      
-    if n > 0 :
-        list_2 = list_1[-n:] +list_1[:-n]
-        print(list_2)
+    n %= len(list_1)
 
-    elif n < 0 :
-        list_2 = list_1[-n:] + list_1[:-n]
-        print(list_2)
+    list_2 = list_1[-n:] + list_1[:-n]
+    print(list_2)
 
-
-    else:
-         break
 
 '''Kod istwnileni veriyor ama liste uzunlugundan  buyuk bir sayi girdigimde hicbir degisiklik yapmiyor. Mod kullanarak islem yapmasi gerekiyor.
  n %= len(list_1)

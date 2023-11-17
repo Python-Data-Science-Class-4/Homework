ccf39aa7f5afbd5fa822ac750108d045cded7cc1
sumeyrazenkin <119689105+sumeyrazenkin@users.noreply.github.com>
2023-11-17 17:43:43 +0100

    Updated by sumeyra

--- WEEK-2 DATA STRUCTURES/6- sevda/dictionary usege.py
+++ WEEK-2 DATA STRUCTURES/6- sevda/dictionary usege.py
@@ -22,3 +22,14 @@ matched_list = [(list1[j],list2[j]) for j in range(listlen) ]
 # Two lists are compared and their elements are matched, each letter matching a number.
 print(matched_list)
 
+
+''' Kod guzel ama sizden isteneni vermiyor. Mesela soyle bir cumle yazdim;
+Write a sentence:bazi eyler VAR ki soyLENMIYOR
+bazi eyler var ki soylenmiyor
+bazieylervarkisoylenmiyor
+['a', 'a', 'b', 'e', 'e', 'e', 'i', 'i', 'i', 'k', 'l', 'l', 'm', 'n', 'o', 'o', 'r', 'r', 'r', 's', 'v', 'y', 'y', 'y', 'z']
+[(1, 'a'), (2, 'a'), (3, 'b'), (4, 'e'), (5, 'e'), (6, 'e'), (7, 'i'), (8, 'i'), (9, 'i'), (10, 'k'), (11, 'l'), (12, 'l'), (13, 'm'), (14, 'n'), (15, 'o'), (16, 'o'), (17, 'r'), (18, 'r'), (19, 'r'), 
+(20, 's'), (21, 'v'), (22, 'y'), (23, 'y'), (24, 'y'), (25, 'z')]
+sizden istenen a hrfinden kac tane varsa onu yazmasi ama siz indexleri yazdirmissiniz.
+Kucuk harfe cevirmeniz ve ozel karakterleri ignore etmeniz cok guzel.'''
+

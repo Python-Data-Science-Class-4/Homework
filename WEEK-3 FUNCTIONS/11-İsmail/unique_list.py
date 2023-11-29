e519741dbe4297af3d0a83ea175d42108806cfc6
sumeyrazenkin <119689105+sumeyrazenkin@users.noreply.github.com>
2023-11-29 14:08:04 +0100

    Updated by sumeyra

--- WEEK-3 FUNCTIONS/11-İsmail/unique_list.py
+++ WEEK-3 FUNCTIONS/11-İsmail/unique_list.py
@@ -11,3 +11,24 @@ def num_filter (given_list):
     
     
 print(num_filter(given_list))
+
+''' Kod iyi ancak bazi gelistirmeler yapabiliriz.
+**Herhangi bir fonksiyon icinde olusturulan degisken 'local variable' olarak adlandirilir,
+fonksiyon disindaki variable'lar ise 'global variable' olarak adlandiriyoruz. 
+Fonksiyon içinde global bir değişkeni (burada unique_list) kullanmak genellikle iyi bir uygulama değildir. 
+Fonksiyonlar genellikle bağımsız ve kendi içerdikleri değişkenleri kullanmalıdır. 
+Bu durumda, fonksiyona parametre olarak given_listi geçirebilir ve işlem sonucunu geri döndürebilirsiniz.
+
+**Fonksiyonunuz şu anda bir şey döndürmüyor,bu nedenle return ifadesine unique_list i ekleyebilirsiniz
+
+**Dongu kullanmadan set() ile de unique elemanlari bulabiliriz. 
+Once set() ile liste elemanlari tekrardan kurtulur, sonra sorted() ile liste halinde sirali dondurulebilir.
+Alternatif bir cozum sundum, inceleyebilirsiniz.
+
+letters = ["a","z","b","c","c","d","f","e","e","g","h","h","j"]
+
+def unrepeated(first_list):
+    last_list = sorted(set(first_list))
+    return last_list
+
+print(unrepeated(letters)) '''

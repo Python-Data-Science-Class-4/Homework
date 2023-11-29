1594548159f8e3bb1b3b84601736b58711dda0ef
sumeyrazenkin <119689105+sumeyrazenkin@users.noreply.github.com>
2023-11-29 14:27:33 +0100

    Updated by sumeyra

--- WEEK-3 FUNCTIONS/14-Furkan/Alphabetic.py
+++ WEEK-3 FUNCTIONS/14-Furkan/Alphabetic.py
@@ -17,3 +17,10 @@ output_clr = colours(x)
 
 print(f'''Input   : {input_clr}
 Output  : {output_clr}''')
+
+''' Kod mantik olarak guzel ve dogru calisiyor ama bir kac duzeltme yapmamiz gerekiyor.
+**'colours' fonksiyonu 'input_clr' adlı bir parametre almalı, 
+ama siz fonksiyonu çağırırken x değişkenini kullanıyorsunuz.Fonksiyon çağrısını düzeltebiliriz.
+**input_clr'yi tanımlarken virgülle ayrılmış bir demet oluşturmalısınız 
+(input_clr = (clr_1, clr_2, clr_3)). 
+Şu anda input_clr bir demet değil üç ayrı değişkeni içeren bir tuple.'''

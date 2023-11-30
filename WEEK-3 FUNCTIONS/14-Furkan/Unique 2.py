44f0267df3aedd1a2f2e1d9e26961a76bcbc4c34
Mafya1903 <150283489+Mafya1903@users.noreply.github.com>
2023-11-30 21:32:28 +0100

    Unique 2.py

--- WEEK-3 FUNCTIONS/14-Furkan/Unique 2.py
+++ WEEK-3 FUNCTIONS/14-Furkan/Unique 2.py
@@ -1,13 +1,9 @@
 
+# Write a function that filters all the unique (unrepeated) elements of a given list.
+
 def remove_duplicates(lst):
     unique_list = list(sorted(set(lst)))
-    print(unique_list)
+    print (unique_list)
 
-numbers = input ("Write a set of numbers: ")
+numbers = [1,2,3,3,3,3,4,5,5]
 remove_duplicates(numbers)
-
-''' Set kullanmaniz cok guzel ama bir kac duzeltme yapabiliriz.. 
-**input fonksiyonu, kullanıcıdan alınan girdiyi bir string olarak alır. 
-Sizin amacınız bir dizi sayı girişi almak. Bu yuzden kullanicinin girdigi sayilari diziye donusturmeniz gerekiyor.
-**Yukarida belirttigim sebepten dolayi kullanici virgulle sayi girisi yaptiginda virgulu de bir sayi gibi algilayip 
-islem yapiyor.'''

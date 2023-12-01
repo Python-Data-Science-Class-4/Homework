f6f1f6965fbdb7be418839264ca22bde352ff65b
Mafya1903 <150283489+Mafya1903@users.noreply.github.com>
2023-12-01 04:34:22 +0100

    Update.py

--- WEEK-3 FUNCTIONS/14-Furkan/Equal 2.py
+++ WEEK-3 FUNCTIONS/14-Furkan/Equal 2.py
@@ -1,24 +1,22 @@
 
-def equal (a,b,c):
+# Write a function that controls the given inputs whether they are equal to their reversed order or not.
+
+def equal (word):
+    return word == word [::-1]
     
-    return word_1 == word_1[::-1], word_2 == word_2[::-1], word_3 == word_3[::-1] 
- 
-word_1 = list (input ("Enter your first word that can be same with its reversed form: ") . lower ())
-word_2 = list (input ("Enter your second word that can be same with its reversed form: ") . lower ())
-word_3 = list (input ("Enter your third word that can be same with its reversed form: ") . lower ())
+word_input = input ("Write a word that can be same with its reversed: ") . lower ()
 
-print (word_1, word_2, word_3)
-print (equal(word_1, word_2, word_3))
+print (word_input)
+print (equal(word_input)) 
 
-'''Sizden istenen soyle bir sey.
-Write a function that controls the given inputs whether they are equal to their reversed order or not.
-Example:
-Input >>> madam, tacocat, utrecht
-Output >>> True, True, False
 
-**Siz kelimeleri harflere ayirmissiniz ama buna gerek yok.
-**Butun kelimlere tek tek ayri islem yapmamak icin fonksiyon yaziyoruz.Ama siz hepsi icin ayri islem yapmissiniz.
-Soruda sizden 3 kelime istemis ama 10 isteseydi daha zor olurdu. Fonksiyonun amacina uygun olarak tekrrar bakalim bu soruya.
 
-**Ayrica equal fonksiyonunun parametrelerini a, b, ve c olarak tanımlanmışsiniz 
-ama fonksiyon içinde kullanılan isimler word_1, word_2, ve word_3. Parametre isimleri ve kullanılan isimlerin aynı olması gerekir'''
+
+
+
+Write a word that can be same with its reversed: madam
+madam
+True
+Write a word that can be same with its reversed: ali
+ali
+False

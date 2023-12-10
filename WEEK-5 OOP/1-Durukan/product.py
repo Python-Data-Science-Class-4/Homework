9cf62a5fb72c3d94968968989958c2bf5bbcfef1
Kemal Durukan MERT <103702848+KemalDurukanMERT@users.noreply.github.com>
2023-12-10 02:27:36 +0100

    1-Durukan

---
+++ WEEK-5 OOP/1-Durukan/product.py
@@ -0,0 +1,23 @@
+class Product():
+    def __init__(self, product_id='',product_name='', product_purchase_price=0, product_sale_price=0):
+        self.product_id=product_id
+        self.product_name=product_name
+        self.product_purchase_price=product_purchase_price
+        self.product_sale_price=product_sale_price
+        self.profit=self.set_remarks()
+
+    def set_remarks(self):
+        if self.product_sale_price - self.product_purchase_price > 0:
+            return 'Profit'
+        elif self.product_sale_price - self.product_purchase_price < 0:
+            return 'Loss'
+        else:
+            return 'None'
+    
+    def show_data(self):
+        print(f'''
+Product ID      : {self.product_id}
+Product Name    : {self.product_name}
+Purchase Price  : {self.product_purchase_price}
+Sale Price      : {self.product_sale_price}
+Profits         : {self.profit}''')
---
+++ WEEK-5 OOP/1-Durukan/society.py
@@ -0,0 +1,41 @@
+class Society():
+    def __init__(self):
+        # Start these functions when Society class called.
+        self.input_data()
+        self.allocate_flat()
+        print(self.show_data())
+
+    def input_data(self):
+        # Take the inputs and assign them to the variables of created object with self parameter.
+        self.society_name = input('Enter your society name: ')
+        self.house_no_of_mem = input('Enter the house number of member: ')
+        while True:
+            try:
+                self.income = int(input('Please enter your income: '))
+                break 
+            except ValueError:
+                print("Invalid input. Income should be an integer")
+
+
+    def allocate_flat(self):
+        # According to the income value of created object assign a flat with if-elif-else block
+        if self.income < 15000:
+            self.flat = "D Type"
+        elif self.income >= 15000 and self.income<20000:
+            self.flat = 'C Type'
+        elif self.income >= 20000 and self.income<25000:
+            self.flat = 'B Type'
+        else:
+            self.flat = 'A Type'
+
+    
+    def show_data(self):
+        # Returns the values of variables to display the values.
+        return f'''
+Society Name    : {self.society_name}
+Number of House : {self.house_no_of_mem}
+Income          : {self.income}
+Flat            : {self.flat}'''
+
+
+society = Society()

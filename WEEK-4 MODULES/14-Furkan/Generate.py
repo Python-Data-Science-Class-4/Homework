cfd55988fd921dd6be20e2dfedaf1a8af16b0290
Mafya1903 <150283489+Mafya1903@users.noreply.github.com>
2023-12-12 23:10:46 +0100

    Generate.py

--- WEEK-4 MODULES/14-Furkan/Generate.py
+++ WEEK-4 MODULES/14-Furkan/Generate.py
@@ -2,21 +2,23 @@ import random
 import string
 
 def new(length):
-
     upper   = string.ascii_uppercase  
     lower   = string.ascii_lowercase 
     numeral = string.digits 
     punct   = string.punctuation
     
-    pw = [random.choice(upper)*2,random.choice(numeral)*2,random.choice(punct)*2]
-    
+    pw_1 = [random.choice(upper),random.choice(numeral),random.choice(punct),
+            random.choice(upper),random.choice(numeral),random.choice(punct)]
+    # Criteria: a) Password length between 10-20   b) At least 2 upper, 2 lower, 2 symbols 
     length = max(10, min(length, 20))
      
-    together = length - len(pw) - 3      # For the formulization I had to use '3'
-    characters = upper + lower + numeral + punct
-    pw.extend(random.choice(characters) for x in range(together))
-    random.shuffle (pw)
-    return ''.join(pw)
+    len_of_others = length - len(pw_1)     
+    other_characters = []
+    for x in range(len_of_others):
+        other_characters += random.choice(lower)
+    pw_1.extend(other_characters)
+    random.shuffle (pw_1)
+    return ''.join(pw_1)
     
 def create():
     password_length = int(input("Enter the number of password between 10-20: "))
@@ -29,12 +31,3 @@ def create():
         print("The number you gave is wrong.")
 
 create()
-
-''' Kod guzel ama neden her karakter grubunu ikiyle carptiginizi anlayamadim. Bu durumda bazı karakterlerin diğerlerinden daha fazla olasılıkla seçilmesine neden olabilir. 
-Yani eğer characters içindeki bir karakter diğer karakterlere göre iki kat daha fazla tekrar ediyorsa o karakterin seçilme olasılığı iki kat daha yüksek olacaktır. 
-Benim denedigim ciktilarda da ardisik olarak cok fazla ayni harfi veriyor mesela.
-Her karakterin eşit olasılıkla seçilmesi icin random.choice(upper) vs. kullanmalisiniz.
-**Karakter gruplarini birleştirirken büyük harf, küçük harf, rakam ve noktalama işaretlerini sırasıyla eklemissiniz.
-Ama bu siralama şifrenin belirli bir karakter setinden önce her zaman aynı iki karakteri icerir. 
-Bunun yerine tüm karakter setlerini birleştirip sonrasinda shuffle etmek daha iyi olur.
-*** Yorum satirlarini da cok az kullanmissiniz,artirirsaniz kodun anlasilirligi da artar.'''
